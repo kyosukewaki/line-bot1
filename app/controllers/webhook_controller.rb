f43873b9913df00e107496968ab70f675e157b83
kyosukewaki <kyosuke.w@gmail.com>
2016-04-13 21:58:50 +0900

    変更

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,25 +1,25 @@
 class WebhookController < ApplicationController
   protect_from_forgery with: :null_session
-    before_action :get_client
+  before_action :get_client
 
-    def callback
-      params[:result].each do |result|
-        from = result[:content][:from]
-        text = result[:content][:text]
-        @client.send_text_message(from, text) if text.present?
-      end
-      render json: [], status: :ok
+  def callback
+    params[:result].each do |result|
+      from = result[:content][:from]
+      text = result[:content][:text]
+      @client.send_text_message(from, text) if text.present?
     end
+    render json: [], status: :ok
+  end
 
-    private
+  private
 
-    def get_client
-      options = {
-        channel_id: ENV['LINE_CHANNEL_ID'],
-        channel_secret: ENV['LINE_CHANNEL_SECRET'],
-        channel_mid: ENV['LINE_CHANNEL_MID'],
-        proxy: ENV['FIXIE_URL'],
-      }
-      @client = LineBotApi::Client.new(options)
-    end
+  def get_client
+    options = {
+      channel_id: ENV['LINE_CHANNEL_ID'],
+      channel_secret: ENV['LINE_CHANNEL_SECRET'],
+      channel_mid: ENV['LINE_CHANNEL_MID'],
+      proxy: ENV['FIXIE_URL'],
+    }
+    @client = LineBotApi::Client.new(options)
+  end
 end
